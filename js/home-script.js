1aabb8dc2c1ef6600989d5a86a96ac4c75928f43
tteets09 <tonyteets123@gmail.com>
2021-01-15 11:21:08 -0500

    Added triangles and animation

--- js/home-script.js
+++ js/home-script.js
@@ -1,14 +1,11 @@
-const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to('.text', {y: '0%', duration: 1, stagger: 0.5});
-tl.to('.slider', {y: '-100%', duration: 1.5, delay: 0.5});
-tl.to('.intro', {y: '-100%', duration: 1}, "-=1.1")
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.5 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1.1");
 
-tl.to('#logo', {x: '3rem', duration: 2}, "-=1");
-tl.to('.divider', {y: '0%', duration: 1}, "-=1");
-tl.to('.links', {y: '0%', duration: .5, stagger: 0.1}, "-=1");
-//tl.fromTo('.links', {opacity: 0}, {opacity: 1, duration: 1, stagger: 0.25});
+tl.to("#logo", { x: "3rem", duration: 2 }, "-=1");
+tl.to(".links", { y: "0%", duration: 0.5, stagger: 0.1 }, "-=1");
 
-
-tl.fromTo('.information', {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
-
+tl.fromTo(".information", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.to(".triangle", { x: 0, duration: 0.5, stagger: 0.12 });
