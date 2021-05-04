e5a079c920d7dc7ebfdbd2b5827c838d5ef1879f
tteets09 <tonyteets123@gmail.com>
2021-05-04 13:12:06 -0400

    Made the move in longer for nav links

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,6 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to("#logo", { x: "3rem", duration: 0.5 }, "");
-tl.to(".links", { y: "0%", duration: 1, stagger: 0.1 }, "-=1");
+tl.to("#logo", { x: "3rem", duration: 0.7 }, "");
+tl.to(".links", { y: "0%", duration: 1.3, stagger: 0.1 }, "-=1");
 
 tl.fromTo(".information", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
